65f80e52529680cc587a751c8a6d296d90ade5bf
hyunDev <var.hyuseo@gmail.com>
2024-01-19 00:21:20 +0900

    가장 많이 받은 선물 문제 풀이

---
+++ .history/가장_많이_받은_선물_20240118231650.js
@@ -0,0 +1,50 @@
+function solution(friends, gifts) {
+    console.log("solution");
+    var answer = 0;
+    // friend 이름으로 배열을 만든다.
+    const giftRecord = new Object();
+    const giftSummary = new Object();
+    friends.forEach((name, index) => {
+        giftRecord[name] = {};
+        giftSummary[name] = {};
+    })
+    // gifts 배열을 돌면서 주고 받은 선물 저장한다.
+    gifts.forEach((name, index) => {
+        const [give, take] = name.split(" ");
+        if(giftRecord[give][take]) {
+            giftRecord[give][take]++;
+        } else {
+            giftRecord[give][take] = 1;
+        }
+    })
+    // friend 이름 별로 준 선물, 받은 선물, 선물 지수 계산된 배열을 만든다.
+    friends.forEach((name, index) => {
+        let givenGift = 0;
+        let takenGift = 0;
+        let giftIndex = 0;
+        friends.forEach((_name, _index) => {
+            givenGift += giftRecord[name][_name];
+            takenGift += giftRecord[_name][name];
+        })
+        giftIndex = givenGift - takenGift;
+        giftSummary[name] = {
+            givenGift: givenGift,
+            takenGift: takenGift,
+            giftIndex: giftIndex
+        }
+    })
+    // friend 이름 별로 받을 선물 계산한다.
+    return answer;
+}
+
+const friends = [];
+const gifts = [];
+
+const addButtonEvent = () => {
+    const button = document.getElementById('startBtn');
+    if (button) {
+        button.addEventListener("click", button, solution(friends, gifts));
+    }
+}
+
+window.addEventListener("load", addButtonEvent);
---
+++ .history/가장_많이_받은_선물_20240118235743.js
@@ -0,0 +1,75 @@
+function solution(friends, gifts) {
+    console.log("solution");
+    var answer = 0;
+    // friend 이름으로 배열을 만든다.
+    const giftRecord = new Object();
+    const giftSummary = new Object();
+    friends.forEach((name, index) => {
+        giftRecord[name] = {};
+        giftSummary[name] = {
+            givenGift: 0,
+            takenGift: 0,
+            giftIndex: 0,
+            nextGift: 0,
+            sameGiftFriendList: []
+        };
+    })
+    // gifts 배열을 돌면서 주고 받은 선물 저장한다.
+    gifts.forEach((name, index) => {
+        const [give, take] = name.split(" ");
+        if(giftRecord[give][take]) {
+            giftRecord[give][take]++;
+        } else {
+            giftRecord[give][take] = 1;
+        }
+    })
+    // friend 이름 별로 준 선물, 받은 선물, 선물 지수 계산된 배열을 만든다.
+    friends.forEach((name, index) => {
+        friends.forEach((_name, _index) => {
+            const given = giftRecord[name][_name] ? giftRecord[name][_name] : 0;
+            const taken = giftRecord[_name][name] ? giftRecord[_name][name] : 0;  
+            giftSummary[name].givenGift += given;
+            giftSummary[name].takenGift += taken;
+            if (given > taken) {
+                giftSummary[name].nextGift++;
+            }
+            if (given === taken) {
+                giftSummary[name]["sameGiftFriendList"].push(_name)
+            }
+        })
+        giftSummary[name].giftIndex = giftSummary[name].givenGift - giftSummary[name].takenGift;
+    })
+
+    const calc = (name, key) => {
+        if (giftSummary[name].giftIndex > giftSummary[key].giftIndex) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // friend 이름 별로 받을 선물 계산한다.
+    Object.keys(giftSummary).forEach((name, index) => {
+        const keys = giftSummary[name]["sameGiftFriendList"];
+        keys.forEach((key, index) => {
+            if (calc(name, key)) {
+                giftSummary[name].nextGift++;
+            }
+        })
+    })
+
+    
+    return answer;
+}
+
+const friends = ["muzi", "ryan", "frodo", "neo"];
+const gifts = ["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"];
+
+const addButtonEvent = () => {
+    const button = document.getElementById('startBtn');
+    if (button) {
+        button.addEventListener("click", () => solution(friends, gifts));
+    }
+}
+
+window.addEventListener("load", addButtonEvent);
